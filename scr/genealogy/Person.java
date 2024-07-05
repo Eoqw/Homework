a4e283d8c604646b562ff790443c0b0eac2a8c03
Lodygin Denis <148810082+Eoqw@users.noreply.github.com>
2024-07-06 01:39:42 +0500

    создали person

--- scr/genealogy/Person.java
+++ scr/genealogy/Person.java
@@ -1,5 +1,36 @@
 package scr.genealogy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Person {
-    
+    private String name;
+    private Person parent;
+    private List<Person> children;
+
+    public Person(String name) {
+        this.name = name;
+        this.children = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Person getParent() {
+        return parent;
+    }
+
+    public void setParent(Person parent) {
+        this.parent = parent;
+    }
+
+    public List<Person> getChildren() {
+        return children;
+    }
+
+    public void addChild(Person child) {
+        child.setParent(this);
+        this.children.add(child);
+    }
 }
