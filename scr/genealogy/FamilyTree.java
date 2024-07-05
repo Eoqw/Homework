6430341fe6d60f2954cc3ee48f3898e48c2b5449
Lodygin Denis <148810082+Eoqw@users.noreply.github.com>
2024-07-06 01:40:13 +0500

    создали древо

--- scr/genealogy/FamilyTree.java
+++ scr/genealogy/FamilyTree.java
@@ -1,5 +1,42 @@
 package scr.genealogy;
 
-public class FamilyTree{
+import java.util.HashMap;
+import java.util.Map;
 
+public class FamilyTree {
+    private Map<String, Person> people;
+
+    public FamilyTree() {
+        this.people = new HashMap<>();
+    }
+
+    public void addPerson(String name) {
+        if (!people.containsKey(name)) {
+            people.put(name, new Person(name));
+        }
+    }
+
+    public Person getPerson(String name) {
+        return people.get(name);
+    }
+
+    public void addChild(String parentName, String childName) {
+        addPerson(parentName);
+        addPerson(childName);
+        Person parent = getPerson(parentName);
+        Person child = getPerson(childName);
+        parent.addChild(child);
+    }
+
+    public void getChildren(String parentName) {
+        Person parent = getPerson(parentName);
+        if (parent != null) {
+            System.out.println("Дети " + parentName + ":");
+            for (Person child : parent.getChildren()) {
+                System.out.println(child.getName());
+            }
+        } else {
+            System.out.println(parentName + " не найден в генеалогическом древе.");
+        }
+    }
 }
