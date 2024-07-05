aa26fde52eda0393399260209e13252f66c60ebb
Lodygin Denis <148810082+Eoqw@users.noreply.github.com>
2024-07-06 01:40:51 +0500

    создали main

--- scr/genealogy/Main.java
+++ scr/genealogy/Main.java
@@ -1,5 +1,15 @@
 package scr.genealogy;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        FamilyTree tree = new FamilyTree();
+        tree.addChild("John", "Michael");
+        tree.addChild("John", "Sarah");
+        tree.addChild("Michael", "Kevin");
+        tree.addChild("Sarah", "Sophie");
+
+        tree.getChildren("John");
+        tree.getChildren("Michael");
+        tree.getChildren("Sarah");
+    }
 }
